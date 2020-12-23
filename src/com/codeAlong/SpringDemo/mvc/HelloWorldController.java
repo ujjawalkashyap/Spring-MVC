73acea12ea4db0ccdd3fa4341a7e8550ec36cf66
ujjawalkashyap <32612153+ujjawalkashyap@users.noreply.github.com>
2020-12-24 00:24:35 +0530

    Request Mapping

--- src/com/codeAlong/SpringDemo/mvc/HelloWorldController.java
+++ src/com/codeAlong/SpringDemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -35,6 +36,20 @@ public class HelloWorldController {
 		
 		//create the message
 		String result = "Yo! "+theName;
+		
+		//add message to the model
+		model.addAttribute("message",result);
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		//convert the data into all caps
+		theName = theName.toUpperCase();
+		
+		//create the message
+		String result = "Namaste! "+theName;
 		
 		//add message to the model
 		model.addAttribute("message",result);
